e85c798088877199cb412fcd829e8b878823bb69
Alice Barbe <alice_barbe@hotmail.com>
2020-01-16 17:43:03 -0500

    oops

--- Task2A.py
+++ Task2A.py
@@ -1,8 +1,6 @@
 # Copyright (C) 2018 Garth N. Wells
 #
 # SPDX-License-Identifier: MIT
-import sys
-sys.path.append("./floodsystem")
 
 from floodsystem.stationdata import build_station_list, update_water_levels
 
