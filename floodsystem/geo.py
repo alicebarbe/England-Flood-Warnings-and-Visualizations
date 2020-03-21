ed5c673001e86f6f950ed8086d4006273e6244d9
Alice Barbe <alice_barbe@hotmail.com>
2020-03-21 13:37:07 -0400

    Cleaning documentation, part 1

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -1,29 +1,28 @@
 # Copyright (C) 2018 Garth N. Wells
 #
 # SPDX-License-Identifier: MIT
-"""This module contains a collection of functions related to
-geographical data.
-
-"""
+"""Collection of functions related to geographical data."""
 
 from haversine import haversine
 from floodsystem.utils import sorted_by_key  # noqa
-from floodsystem.stationdata import build_station_list
 
 
 def stations_by_distance(stations, p):
-    """Calculate distances from a coordinate p to each station. Return the station
-    distance pairs in an ordered list, by distance.
+    """Calculate distances from stations to a coordinate p.
 
-    Arguments:
-        stations: (list of MonitoringStation).
-            generated using build_station_list.
-        p: (lat, lon).
-            float coordinates of the center.
-            
-    Returns:
-        (station, distance): (MonitoringStation, float).
-            pair of station object and the corresponding distance from it to point p
+    Parameters
+    ----------
+    stations : list of MonitoringStation
+        generated using build_station_list.
+    p : TYPE
+        float coordinates of the center.
+
+    Returns
+    -------
+    (MonitoringStation, float)
+        pair of station object and the corresponding distance from it to point
+        p.
+
     """
     output = []
     for station in stations:
@@ -34,18 +33,21 @@ def stations_by_distance(stations, p):
 
 def stations_within_radius(stations, centre, r):
     """Return station/distance pair that are within radius r of center.
-    
-    Arguments:
-        stations: (list of MonitoringStation).
-            generated using build_station_list.
-        centre: (lat, lon) (float, float).
-            float coordinates of the center.
-        r (float):
-            
-            
-    Returns:
-        list of station: ([MonitoringStation]).
-            list of station objects
+
+    Parameters
+    ----------
+    stations : list of MonitoringStation
+        generated using build_station_list.
+    centre : (float, float)
+        float coordinates of the center.
+    r : float
+        radius from center.
+
+    Returns
+    -------
+    output : ([MonitoringStation])
+        list of station objects.
+
     """
     output = []
     for pair in stations_by_distance(stations, centre):
@@ -56,17 +58,19 @@ def stations_within_radius(stations, centre, r):
 
 
 def rivers_with_stations(stations):
-    """Return a set containing names of rivers with an associated monitoring station
+    """Get names of rivers with an associated monitoring station.
 
-    Arguments:
-        stations: (list of MonitoringStation).
-            generated using build_station_list.
+    Parameters
+    ----------
+    stations : (list of MonitoringStation)
+        generated using build_station_list.
 
-    Returns:
-        {river_name}: {string}.
-            A set containing the names of all the rivers with a monitoring station
+    Returns
+    -------
+    output : set
+        A set containing the names of all the rivers with a monitoring station.
+
     """
-
     output = set()
     for station in stations:
         # if the station has a river, add to the set
@@ -77,17 +81,20 @@ def rivers_with_stations(stations):
 
 
 def stations_by_river(stations):
-    """Maps stations to the rivers which they are associated with. Return a dict
-    containing MonitoringStations with key values for each river
+    """Map stations to the rivers which they are associated with.
 
-    Arguments:
-        stations: (list of MonitoringStation).
-            generated using build_station_list.
+    Parameters
+    ----------
+    stations : list of MonitoringStation
+        generated using build_station_list..
 
-    Returns:
+    Returns
+    -------
+    output : dict()
         {river_name : [MonitoringStations]}: {string : [MonitoringStation]}.
-            dict containing lists of MonitoringStations for each river. The dict
-            keys are the river names
+        dict containing lists of MonitoringStations for each river. The dict
+        keys are the river names.
+
     """
     output = {}
     rivers = rivers_with_stations(stations)
@@ -104,31 +111,34 @@ def stations_by_river(stations):
     return output
 
 
-def rivers_by_station_number(stations, N):
-    """Finds the N rivers with the greatest number of stations. the rivers are
-    returned in order of decreasing number of stations. If there is a tie between
-    multiple rivers at the Nth entry, all rivers with equal number of stations
-    are returned
+def rivers_by_station_number(stations_list, N):
+    """Find the N rivers with the greatest number of stations.
 
-    Arguments:
-        stations: (list of MonitoringStation).
-            generated using build_station_list.
+    The rivers are returned in order of decreasing number of stations.
+    If there is a tie between multiple rivers at the Nth entry, all rivers
+    with equal number of stations are returned.
 
-        N: (int).
-            The number of rivers to return, in descending order of number of
-            stations
+    Parameters
+    ----------
+    stations : list of MonitoringStation
+        generated using build_station_list.
+    N : int
+        The number of rivers to return, in descending order of number of
+        stations
 
-    Returns:
-        (river_name, number_of_stations): (string, int).
-            list of tuples containing the river names and number of stations
-            of rivers with the N highest number of monitoring stations
+    Returns
+    -------
+    list[(string, int)]
+        list of tuples containing the river names and number of stations
+        of rivers with the N highest number of monitoring stations.
+
     """
     river_station_nums = []
-    river_stations = stations_by_river(stations)
+    river_stations = stations_by_river(stations_list)
 
-    # find the number of stations next to each river, then sort rivers by this number
+    # find the number of stations next to each river, then sort rivers
     for river, stations in river_stations.items():
-        # Append to the list tuples of the form (river_name, number_of_stations)
+        # Append to the list tuples of the form (river_name,number_of_stations)
         river_station_nums.append((river, len(stations)))
     output = sorted_by_key(river_station_nums, 1, reverse=True)
 
